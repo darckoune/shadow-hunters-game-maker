8f954e847d54985b6baf0bd7c9d91aa7f8e9ec06
Darckoune <darckoune@gmail.com>
2020-04-23 18:12:15 +0200

    convert images to jpg

---
+++ convert_images.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#convert
+for image in uncompressed_cards_images/*.png; do
+        dst="${image%.png}.jpg"
+        dst="public/cards/images/${dst##*/}"
+        convert  "$image"  "$dst"
+        echo “image $image converted to ${dst}”
+done
+exit 0 
