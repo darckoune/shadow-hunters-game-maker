a3e5e58d95e1d4291423ca9c8dcdc46794668d20
Darckoune <darckoune@gmail.com>
2020-05-02 22:46:37 +0200

    transform images to webp

--- convert_images.sh
+++ convert_images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #convert
 for image in uncompressed_cards_images/*.png; do
-        dst="${image%.png}.jpg"
+        dst="${image%.png}.webp"
         dst="public/cards/images/${dst##*/}"
         convert  "$image"  "$dst"
         echo “image $image converted to ${dst}”
